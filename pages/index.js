d65e9189fbb2fc5820da3a0e7cf7b5f93789bc8b
Rob Moore <83732271+RWMoo@users.noreply.github.com>
2022-04-25 23:43:27 +0930

    fixed graphql dependency error

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
 import PageLayout from "../components/layouts/PageLayout"
 import MainCard from "../components/Cards/MainCard";
 import LatestPost from "../components/Cards/LatestPostCard";
+import { gql, GraphQLClient } from "graphql-request";
 
-
+export const getStaticProps = async () => {
+  const graphcms = new GraphQLClient("https://api-ap-southeast-2.graphcms.com/v2/ckxh59qbh0wsm01xoayv9051z/master");
+  const { posts } = await graphcms.request(
+    gql`
+      query MyQuery {
+        posts {
+          id
+          slug
+          title
+          date
+          excerpt
+          coverImage {
+            url
+            alt
+          }
+          category {
+            title
+          }
+          minutes
+        }
+      }
+    `
+  ).catch(e => console.log(e))
+  return {
+    props: {
+      posts,
+    },
+  };
+};
 
 const Home = ({ posts }) => {
   return (
@@ -15,14 +44,16 @@ const Home = ({ posts }) => {
         </p>
       </div>
       <section className="mt-8">
-       
+        <MainCard {...posts[0]} />
       </section>
       <section>
         <h2 className="text-2xl ml-2 font-semibold mt-8 text-primary font-display ">
           Latest posts
         </h2>
         <div className="space-y-4 mt-8">
-         
+          {posts.slice(1).map((item) => (
+            <LatestPost key={item.id} {...item} />
+          ))}
         </div>
       </section>
     </PageLayout>
